a279333ff9ad9877d9c50e9d2c8fa55683d9b2d2
Aldairhm <stevenaldairh@gmail.com>
2025-11-22 20:05:55 -0600

    bbbb

--- config/server.php
+++ config/server.php
@@ -5,7 +5,7 @@
 //	const DB_USER="Josue";
 //	const DB_PASS='gj23001';
 // steven
-onst DB_SERVER="localhost";
-	const DB_NAME="db";
+    const DB_SERVER="localhost";
+	const DB_NAME="bd";
 	const DB_USER="root";
 	const DB_PASS='root';
