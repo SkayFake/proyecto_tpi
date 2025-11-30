66874c4f94e80a3680c4cff63953d94f964e9fd5
SkayFake <steven19denoviembre@gmail.com>
2025-11-30 00:07:13 -0600

    a

--- app/controllers/expedienteController.php
+++ app/controllers/expedienteController.php
@@ -3,7 +3,7 @@
 header('Content-Type: application/json; charset=utf-8');
 
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../models/ExpedienteModel.php';
+require_once __DIR__ . '/../models/expedienteModel.php';
 
 $exp = new ExpedienteModel();
 $action = $_GET['action'] ?? null;
